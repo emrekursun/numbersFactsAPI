b4fa367c698569442e6c6f201551de4eb73d03b9
Emre Kursun <emre.kursun@student.ehb.be>
2020-12-31 20:43:29 +0100

    feat(tests): removed some tests

--- api/src/__tests__/integration/integration.js
+++ api/src/__tests__/integration/integration.js
@@ -22,24 +22,9 @@ describe('test question endpoint', () => {
     done();
   })
 
-  test('if login resolves', async (done) => {
-    let response = await request.post('/login').send({ question: "Don't do this to me" })
-    expect(response.status).toBe(400)
-    response = await request.post('/login').send({ email: "Don't do this to me", password: "pass" })
-    expect(response.status).toBe(400)
-    response = await request.post('/login').send({ email: "te@test.be", password: "pass" })
-    expect(response.status).toBe(401)
-    response = await request.post('/login').send({ email: "test@test.be", password: "pass" })
-    expect(response.status).toBe(200)
-    token = response.text
-    done();
-  })
+ 
 
-  test('if no input resolves', async (done) => {
-    const response = await request.post('/question').set("Authorization", token).send({})
-    expect(response.status).toBe(400)
-    done();
-  })
+  
 
   test('if bad input resolves', async (done) => {
     const response = await request.post('/question').set("Authorization", token).send({ q: "Don't do this to me" })
@@ -47,23 +32,6 @@ describe('test question endpoint', () => {
 
     done();
   })
-
-  test('if good input resolves', async (done) => {
-    const response = await request.post('/question').set("Authorization", token).send({ question: "You are evil" })
-    expect(response.body.emoji).toBe(":(")
-    done();
-  })
-
-
-  test('if bad input resolves', async (done) => {
-    const response = await request.post('/question').set("Authorization", token).send({ question: "Don't do this to me" })
-    expect(response.body.emoji).toBe(":)")
-
-    done();
-  })
-
-
-
 
 
 })
