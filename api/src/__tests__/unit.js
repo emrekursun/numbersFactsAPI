d7951b04712f1f8d881f07cb6727248869aee635
Emre Kursun <emre.kursun@student.ehb.be>
2020-12-31 22:53:58 +0100

    feat(DBconnection): connection established

--- api/src/__tests__/unit.js
+++ api/src/__tests__/unit.js
@@ -1,24 +1,45 @@
+const Helpers = require('./../helper/AuthHelper.js')
 
-const ConversationhelpersImport = require('../helper/ConversationHelpers.js')
+describe('helpers test', () => {
+    test('check if function generates something', () => {
+        expect(Helpers.generateUUID()).not.toBeUndefined();
+    })
+    test('check if generated is UUID', () => {
+        expect(Helpers.generateUUID()).toMatch(/\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/)
+    })
 
-describe('check if helper senses emotion', () => {
-  test('check on bad input', () => {
-    expect(ConversationhelpersImport.senseEmotionHelper(null)).toBe(null);
-    expect(ConversationhelpersImport.senseEmotionHelper(123)).toBe(null);
-    expect(ConversationhelpersImport.senseEmotionHelper("")).toBe(null);
-    expect(ConversationhelpersImport.senseEmotionHelper("a")).toBe(null);
-  })
-  test('check on no question', () => {
-    expect(ConversationhelpersImport.senseEmotionHelper('hi there.')).toBe(1);
-  })
-  test('check if good comes through', () => {
-    expect(ConversationhelpersImport.senseEmotionHelper("Don't you like this?")).toBe(2);
-    expect(ConversationhelpersImport.senseEmotionHelper("Do you like this?")).toBe(2);
-  })
+    
+}) 
 
-  test('interpret emotion value', () => {
-    expect(ConversationhelpersImport.convertEmotionValue(0)).toHaveProperty("emoji", ":|")
-    expect(ConversationhelpersImport.convertEmotionValue(-1)).toHaveProperty("emoji", ":(")
-    expect(ConversationhelpersImport.convertEmotionValue(1)).toHaveProperty("emoji", ":)")
-  })
+describe('limit tests', () => { 
+    test('requires one argument', () => {
+        expect(Helpers.limitToTen()).toBeUndefined()
+    })
+    test('if only accepts arrays', () => {
+        expect(Helpers.limitToTen('string voorbeeld')).toBeUndefined()
+        expect(Helpers.limitToTen([])).not.toBeUndefined()
+    })
+    test('if only return 10 items', () => {
+        expect(Helpers.limitToTen([1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,0 ,11 , 12]).length).toBe(10)
+    })
+})
+
+
+
+describe('lengthCheck test', () => {
+    test('moet een string zijn', () => {
+        expect(Helpers.lengthCheck(1201)).toBeFalsy()
+        expect(Helpers.lengthCheck(-201)).toBeFalsy()
+        expect(Helpers.lengthCheck(null)).toBeFalsy()
+        expect(Helpers.lengthCheck([])).toBeFalsy()
+        expect(Helpers.lengthCheck("string")).toBeDefined()
+    })
+    test('length of string kan niet meer dan 100 zijn', () =>{
+        expect(Helpers.lengthCheck("Een ietwat langere string")).toBeTruthy()
+        expect(Helpers.lengthCheck("Een ietwat langere string en ietwat langere string en ietwat langere")).toBeTruthy()
+    })
+    test('moet beginnen met hoofdletter', () =>{
+        expect(Helpers.lengthCheck("Een ietwat langere string")).toBeTruthy()
+        expect(Helpers.lengthCheck("Een ietwat langere string")).toBeTruthy()
+    })
 })
