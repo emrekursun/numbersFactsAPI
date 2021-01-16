1c365afaefd96532219a7cfc51ab7a0c48672496
Emre Kursun <emre.kursun@student.ehb.be>
2021-01-17 00:27:45 +0100

    hotfix(unittest): fixed unit test v2

--- api/src/__tests__/unit.js
+++ api/src/__tests__/unit.js
@@ -11,35 +11,4 @@ describe('helpers test', () => {
     
 }) 
 
-describe('limit tests', () => { 
-    test('requires one argument', () => {
-        expect(Helpers.limitToTen()).toBeUndefined()
-    })
-    test('if only accepts arrays', () => {
-        expect(Helpers.limitToTen('string voorbeeld')).toBeUndefined()
-        expect(Helpers.limitToTen([])).not.toBeUndefined()
-    })
-    test('if only return 10 items', () => {
-        expect(Helpers.limitToTen([1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,0 ,11 , 12]).length).toBe(10)
-    })
-})
 
-
-
-describe('lengthCheck test', () => {
-    test('moet een string zijn', () => {
-        expect(Helpers.lengthCheck(1201)).toBeFalsy()
-        expect(Helpers.lengthCheck(-201)).toBeFalsy()
-        expect(Helpers.lengthCheck(null)).toBeFalsy()
-        expect(Helpers.lengthCheck([])).toBeFalsy()
-        expect(Helpers.lengthCheck("string")).toBeDefined()
-    })
-    test('length of string kan niet meer dan 100 zijn', () =>{
-        expect(Helpers.lengthCheck("Een ietwat langere string")).toBeTruthy()
-        expect(Helpers.lengthCheck("Een ietwat langere string en ietwat langere string en ietwat langere")).toBeTruthy()
-    })
-    test('moet beginnen met hoofdletter', () =>{
-        expect(Helpers.lengthCheck("Een ietwat langere string")).toBeTruthy()
-        expect(Helpers.lengthCheck("Een ietwat langere string")).toBeTruthy()
-    })
-})
